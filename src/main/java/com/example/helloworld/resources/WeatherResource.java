ef1bc23247b942b5143b6b867e604d006f9024fa
Jenna Kwon <JennaKwon@ipsec-143-215-17-198.vpn.gatech.edu>
2016-08-28 18:22:51 -0400

    modified database setting

--- src/main/java/com/example/helloworld/resources/WeatherResource.java
+++ src/main/java/com/example/helloworld/resources/WeatherResource.java
@@ -1,7 +1,40 @@
 package com.example.helloworld.resources;
 
-/**
- * Created by JennaKwon on 8/27/16.
- */
+import com.example.helloworld.core.Temperature;
+import com.example.helloworld.db.TemperatureDAO;
+import io.dropwizard.hibernate.UnitOfWork;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/weather")
+@Produces(MediaType.APPLICATION_JSON)
 public class WeatherResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldResource.class);
+
+    private final TemperatureDAO temperatureDAO;
+
+
+    public WeatherResource(TemperatureDAO temperatureDAO) {
+        this.temperatureDAO = temperatureDAO;
+    }
+
+    @POST
+    @UnitOfWork
+    public Temperature createTemperature(Temperature temperature) {
+        return temperatureDAO.create(temperature);
+    }
+
+    @GET
+    @UnitOfWork
+    public List<Temperature> listTemperature() {
+        return temperatureDAO.findAll();
+    }
+
 }
